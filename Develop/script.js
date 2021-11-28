ae9e610f1c6dc3ca333b532d36c479cf1c97950f
svnday <40030806+svnday@users.noreply.github.com>
2021-11-27 19:29:15 -0600

    functions for getting letters and random numbers and symbols

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const passwordLength = document.getElementById('length');
+
+const randomVariables = {
+  lowercase: getRandomLowercase,
+  uppercase: getRandomUppercase,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+}
+
+function getRandomLowercase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUppercase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) +48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*<>/';
+  return symbols[Math.floor(math.random() * symbols.length)];
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
